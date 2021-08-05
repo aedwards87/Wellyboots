fabbd61d8382233fde352a1727c2b5997d2b5ee4
Adam Edwards <aedwards1987@gmail.com>
2021-08-06 00:49:09 +0100

    some placeholder data

---
+++ web/src/data/footerLinks.js
@@ -0,0 +1,25 @@
+const footerLinks = [
+  {
+    title: 'Play theory',
+  },
+  {
+    title: 'Locations',
+  },
+  {
+    title: 'Cirriculum',
+  },
+  {
+    title: 'Reviews',
+  },
+  {
+    title: 'About us',
+  },
+  {
+    title: 'Contact us',
+  },
+  {
+    title: 'Policies & Procedures',
+  },
+]
+
+export default footerLinks
