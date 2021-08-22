b7969f40a8de3836957694dc265ff1898c6d7c57
Adam Edwards <aedwards1987@gmail.com>
2021-08-22 14:41:32 +0100

    took the logic out from hero and review components and created a hook to tidy things up

---
+++ web/src/hooks/usePaginate.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import { wrap } from 'popmotion';
+
+export const usePaginate = (data, timer) => {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const carouselIndex = wrap(0, data.length, page);
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
+  // Timer to loop through data
+  if (timer) {
+    useEffect(() => {
+      const Timer = setInterval(() => {
+        paginate(1)
+      }, timer)
+      return () => clearInterval(Timer)
+    })
+  }
+
+  return [page, direction, paginate, carouselIndex]
+}
