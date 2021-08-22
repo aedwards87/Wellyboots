b7e6e26b4b68fe584e543e2971ba58bf8a14e16d
Adam Edwards <aedwards1987@gmail.com>
2021-08-22 14:51:29 +0100

    updated to include new hooks created for export

--- web/src/hooks/index.js
+++ web/src/hooks/index.js
@@ -1,9 +1,10 @@
 export * from './useStore';
 export * from './useClickOutside';
+export * from './useHoverToggle';
 export * from './useToggle';
-export * from './useNavToggle';
 export * from './useMediaQuery';
 export * from './useStore';
+export * from './usePaginate';
 
 
 // Context
@@ -12,5 +13,9 @@ export * from './Context/useNavMenuToggleContext';
 
 
 // Queries
-export * from './Queries/useCompanyLocationsQuery';
+export * from './Queries/useLocationsQuery';
 export * from './Queries/useCompanyBrandQuery';
+export * from './Queries/useSiteDetailsQuery';
+export * from './Queries/useAboutUsQuery';
+export * from './Queries/useReviewsQuery';
+export * from './Queries/useHeroQuery';
