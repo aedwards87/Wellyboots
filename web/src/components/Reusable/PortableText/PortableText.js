e65ac8a8e238c4098eecb439720c54924965b31b
Adam Edwards <aedwards1987@gmail.com>
2021-08-22 14:29:10 +0100

    updated files to work together

--- web/src/components/Reusable/PortableText/PortableText.js
+++ web/src/components/Reusable/PortableText/PortableText.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import BasePortableText from '@sanity/block-content-to-react';
 import clientConfig from '../../../../client-config';
-// import Serializers from './Serializers';
+import Serializers from './Serializers';
+import styled from 'styled-components';
 
-const PortableText = ({ blocks }) => (
-  <BasePortableText
-    blocks={blocks}
-    // serializers={Serializers}
-    {...clientConfig.sanity}
-  />
+const PortableText = ({ className, blocks, ...props }) => (
+  <Container className={className} {...props}>
+    <BasePortableText
+      blocks={blocks}
+      serializers={Serializers}
+      {...clientConfig.sanity}
+    />
+  </Container>
 );
+
+const Container = styled.div``
 
 export default PortableText;
--- web/src/components/Reusable/PortableText/Serializers.js
+++ web/src/components/Reusable/PortableText/Serializers.js
@@ -1,11 +1,62 @@
 import React from 'react';
-// import Figure from './Figure'
+import BlockContent from "@sanity/block-content-to-react";
+import Link from '../Link';
 
 const serializers = {
   types: {
-    authorReference: ({ node }) => <span>{node.author.name}</span>,
-    // mainImage: Figure
+    block: (props) => {
+      const { style = "normal" } = props.node;
+
+      if (/^h\d/.test(style)) {
+        const level = style.replace(/[^\d]/g, "");
+        return React.createElement(
+          style,
+          { className: `heading-${level}` },
+          props.children
+        );
+      }
+
+      if (style === "blockquote") {
+        return <blockquote>- {props.children}</blockquote>;
+      }
+
+      // Fall back to default handling
+      return BlockContent.defaultSerializers.types.block(props);
+    },
+    code: (props) =>
+      console.log("code block", props) || (
+        <pre data-language={props.node.language}>
+          <code>{props.node.code}</code>
+        </pre>
+      )
   },
+  list: (props) =>
+    console.log("list", props) ||
+    (props.type === "bullet" ? (
+      <ul>{props.children}</ul>
+    ) : (
+      <ol>{props.children}</ol>
+    )),
+  listItem: (props) =>
+    console.log("list", props) ||
+    (props.type === "bullet" ? (
+      <li>{props.children}</li>
+    ) : (
+      <li>{props.children}</li>
+    )),
+  marks: {
+    strong: (props) =>
+      console.log("strong", props) || <strong>{props.children}</strong>,
+    em: (props) => console.log("em", props) || <em>{props.children}</em>,
+    code: (props) => console.log("code", props) || <code>{props.children}</code>,
+    link: (props) =>
+      props.mark.href.includes('www.') ? (
+        <a href={props.mark.href}>{props.children}</a>
+      ) : (
+        <Link to={props.mark.href}>{props.children}</Link>
+      )
+    ,
+  }
 };
 
 export default serializers;
