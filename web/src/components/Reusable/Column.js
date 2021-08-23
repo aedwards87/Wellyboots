ce0e046350040455578d81edfc9adfaedf761cf7
Adam Edwards <aedwards1987@gmail.com>
2021-08-23 02:00:09 +0100

    added gap styling

--- web/src/components/Reusable/Column.js
+++ web/src/components/Reusable/Column.js
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components/macro'
 import { regExpLetterChecker } from '../../utils/helpers'
 
 
-const Column = ({ className, children, xGap, yGap, columnOrder, xAlign = "start", xItemsAlign, yAlign, yItemsAlign, rowOrder, innerRef, ...props }) => {
+const Column = ({ className, children, gap, xGap, yGap, columnOrder, xAlign = "start", xItemsAlign, yAlign, yItemsAlign, rowOrder, innerRef, ...props }) => {
   return (
     <Container
       className={className}
@@ -12,6 +12,8 @@ const Column = ({ className, children, xGap, yGap, columnOrder, xAlign = "start"
       style={{
         '--columnXGap': regExpLetterChecker(xGap) ? [xGap] : [`var(--s${xGap})`, `var(--s${xGap + 2})`],
         '--columnYGap': regExpLetterChecker(yGap) ? [yGap] : [`var(--s${yGap})`, `var(--s${yGap + 2})`],
+        '--columnYGap': regExpLetterChecker(yGap) ? [yGap] : [`var(--s${yGap})`, `var(--s${yGap + 2})`],
+        '--columnGap': regExpLetterChecker(gap) ? [gap] : [`var(--s${gap})`, `var(--s${gap + 2})`],
         '--xAlign': xAlign,
         '--xItemsAlign': xItemsAlign,
         '--yAlign': yAlign,
@@ -30,6 +32,7 @@ const Container = styled(motion.div)`
   display: grid;
   ${({ style }) => style['--columnYGap'][0] && css`grid-row-gap: ${style['--columnYGap'][0]};`}
   ${({ style }) => style['--columnXGap'][0] && css`grid-column-gap: ${style['--columnXGap'][0]};`}
+  ${({ style }) => style['--columnGap'][0] && css`gap: ${style['--columnGap'][0]};`}
   ${({ style }) => style['--xItemsAlign'] && css`justify-items: var(--xItemsAlign);`}
   ${({ style }) => style['--xAlign'] && css`justify-content: var(--xAlign);`}
   ${({ style }) => style['--yItemsAlign'] && css`align-items: var(--yItemsAlign);`}
