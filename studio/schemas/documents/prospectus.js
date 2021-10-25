e63030953938a8f523eff958fb1a4aaff4cc3d9c
Adam Edwards <aedwards1987@gmail.com>
2021-10-25 02:25:57 +0100

    component to download prospectus

---
+++ studio/schemas/documents/prospectus.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'prospectus',
+  title: 'Prospectus',
+  type: 'document',
+  fields: [
+    {
+      name: 'prospectus',
+      title: 'Prospectus',
+      type: 'file',
+      description: 'Upload prospectus'
+    },
+  ]
+}
