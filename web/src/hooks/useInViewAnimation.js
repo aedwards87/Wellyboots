3980babaca3481cd43483aaaafdb948983d8f2a1
Adam Edwards <aedwards1987@gmail.com>
2021-11-22 00:51:56 +0000

    removed unused code

--- web/src/hooks/useInViewAnimation.js
+++ web/src/hooks/useInViewAnimation.js
@@ -1,13 +1,13 @@
 import { useAnimation } from "framer-motion";
-import React, { useLayoutEffect, useState } from "react";
-import { useInView, InView } from "react-intersection-observer";
-export const useInViewAnimation = (threshold = 0) => {
+import { useLayoutEffect } from "react";
+import { useInView } from "react-intersection-observer";
+export const useInViewAnimation = ({ threshold = 0 }) => {
   const controls = useAnimation();
   const [ref, inView] = useInView({
     threshold: threshold
   });
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (inView) {
       controls.start("visible");
     }
@@ -15,27 +15,3 @@ export const useInViewAnimation = (threshold = 0) => {
 
   return { ref, inView, controls }
 }
-
-export const InViewAnimation = ({ Component, className, ...props }) => {
-  const controls = useAnimation();
-  const [isInView, setInView] = useState()
-
-  return (
-    <InView>
-      {({ inView, ref }) =>
-        <Title
-          className={className}
-          ref={ref}
-          initial={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-          animate={inView ? { opacity: 1 } : { opacity: 0 }}
-          {...props}
-        >
-          {children}
-        </Title>
-      }
-    </InView>
-  )
-}
-
-
