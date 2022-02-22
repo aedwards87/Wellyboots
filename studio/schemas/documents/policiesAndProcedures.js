edf3c680084c076e8251b4152800ba9e7544897a
Adam Edwards <aedwards1987@gmail.com>
2022-02-22 00:15:11 +0000

    updated schemas to better accomodate copy

--- studio/schemas/documents/policiesAndProcedures.js
+++ studio/schemas/documents/policiesAndProcedures.js
@@ -12,9 +12,20 @@ export default {
       title: 'Title'
     },
     {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      description: 'A slug is the part of a URL which identifies a particular page on a website',
+      options: {
+        source: 'title',
+        maxLength: 96
+      },
+      validation: Rule => Rule.required().error('A slug is required for this document to have it\'s own page')
+    },
+    {
       name: 'description',
       type: 'bodyPortableText',
       title: 'Description'
-    }
+    },
   ]
 }
